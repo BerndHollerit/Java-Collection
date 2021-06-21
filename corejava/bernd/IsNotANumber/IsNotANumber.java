cb64cf545e8eef3648479ee5456030534cf01ccc
Bernd Hollerit <hollerit@gmail.com>
2021-06-21 20:57:57 +0900

    Replace separate numbers with array

--- corejava/bernd/IsNotANumber/IsNotANumber.java
+++ corejava/bernd/IsNotANumber/IsNotANumber.java
@@ -1,44 +1,32 @@
 /**
  * Exercise of Core Java Chapter 3, page 45
+ * 
  * @version 1.00 2021-06-21
  * @author Bernd Hollerit
  */
-public class IsNotANumber
-{
-   static double x = Double.NaN;
-   static double y = 0.0;
-   static double z = 42;
-   static double a = Double.POSITIVE_INFINITY;
-   static double b = Double.NEGATIVE_INFINITY;
-   
-   public static void main(String[] args)
-   {
-		isItANumber(x);
-		isItANumber(y);
-		isItANumber(z);
-		isItANumber(a);
-		isItANumber(b);
-   }
-   
-   public static void isItANumber(double x)
-   {
-		if (x == Double.NaN)
-		{
-			System.out.println("We cannot reach this");
+public class IsNotANumber {
+
+	static double[] array = { Double.NaN, 0.0, 42, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY };
+
+	public static void main(String[] args) {
+		isItANumberArray(array);
+	}
+
+	public static void isItANumberArray(double[] x) {
+		for (int i = 0; i < x.length; i++) {
+
+			if (x[i] == Double.NaN) {
+				System.out.println("We cannot reach this");
+			} else {
+				System.out.println("Is never true");
+			}
+
+			if (Double.isNaN(x[i])) {
+				System.out.println("Is not a number");
+			} else {
+				System.out.print("The number is ");
+				System.out.println(x[i]);
+			}
 		}
-		else
-		{
-			System.out.println("Is never true");
-		}
-		
-		if (Double.isNaN(x))
-		{
-			System.out.println("Is not a number");
-		}
-		else
-		{
-			System.out.print("The number is ");
-			System.out.println(x);
-		}
-   }
+	}
 }

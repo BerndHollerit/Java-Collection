21f10bfa2156dcf31bc197f1e4ad701dd677944c
Bernd Hollerit <hollerit@gmail.com>
2021-06-21 20:31:22 +0900

    Add program logic

--- corejava/bernd/IsNotANumber/IsNotANumber.java
+++ corejava/bernd/IsNotANumber/IsNotANumber.java
@@ -7,6 +7,25 @@ public class IsNotANumber
 {
    public static void main(String[] args)
    {
-      System.out.println("Is not a number");
+		double x = Double.NaN;
+		
+		if (x == Double.NaN)
+		{
+			System.out.println("We cannot reach this");
+		}
+		else
+		{
+			System.out.println("Is never true");
+		}
+		
+		if (Double.isNaN(x))
+		{
+			System.out.println("x is not a number");
+		}
+		else
+		{
+			System.out.print("x is ");
+			System.out.println(x);
+		}
    }
 }

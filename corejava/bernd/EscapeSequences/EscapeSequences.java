24a226355abbc168595a6f3aa0c470410be1307d
Bernd Hollerit <hollerit@gmail.com>
2021-06-21 21:16:18 +0900

    Create Escape Sequences

---
+++ corejava/bernd/EscapeSequences/EscapeSequences.java
@@ -0,0 +1,19 @@
+/**
+ * Exercise of Core Java Chapter 3, page 45
+ * 
+ * @version 1.00 2021-06-21
+ * @author Bernd Hollerit
+ */
+public class EscapeSequences {
+	public static void main(String[] args) {
+		System.out.println("Backspace: \b before this");
+		System.out.println("Tab: \t before this");
+		System.out.println("Linefeed: \nbefore this");
+		System.out.println("   Carriage return: \rCR");
+		System.out.println("Double Quote: \"");
+		System.out.println("Single Quote: \'");
+		System.out.println("Backslash: \\");
+		System.out.println("Trademark symbol: \u2122");
+		System.out.println("Pi: \u03C0");
+	}
+}

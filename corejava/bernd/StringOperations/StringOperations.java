d8f0e445cb95211fd440c3b587d25713d20b76b3
Bernd Hollerit <hollerit@gmail.com>
2021-06-23 14:52:27 +0900

    Add StringOperations

---
+++ corejava/bernd/StringOperations/StringOperations.java
@@ -0,0 +1,38 @@
+/**
+ * Exercise of Core Java Chapter 3, page 62-68
+ * 
+ * @version 1.00 2021-06-23
+ * @author Bernd Hollerit
+ */
+public class StringOperations {
+
+	
+
+	public static void main(String[] args) {
+		String greeting = "Hello";
+		System.out.println(greeting);
+		String s = greeting.substring(0,4);
+		System.out.println(s);
+		String sizes = String.join(" / ", "S", "M", "L", "XL");
+		System.out.println(sizes);
+		
+		if ("Hello".equals(greeting)) {
+			System.out.println("Equals");
+		}
+		
+		String empty = "";
+		
+		if (empty.length() == 0)
+			System.out.println("Length of String is 0");
+			
+		if (empty.equals(""))
+			System.out.println("String equals \"\"");
+			
+		if (empty != null && empty.length() != 0)
+			System.out.println("String is not null and its length is not 0. String: " + empty);
+
+
+	}
+
+
+}

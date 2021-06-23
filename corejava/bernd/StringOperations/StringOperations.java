5091837cc98f0706c29f0060d52e15975b372b1a
Bernd Hollerit <hollerit@gmail.com>
2021-06-23 14:53:34 +0900

    Update whitespace formatting

--- corejava/bernd/StringOperations/StringOperations.java
+++ corejava/bernd/StringOperations/StringOperations.java
@@ -6,33 +6,29 @@
  */
 public class StringOperations {
 
-	
-
 	public static void main(String[] args) {
 		String greeting = "Hello";
 		System.out.println(greeting);
-		String s = greeting.substring(0,4);
+		String s = greeting.substring(0, 4);
 		System.out.println(s);
 		String sizes = String.join(" / ", "S", "M", "L", "XL");
 		System.out.println(sizes);
-		
+
 		if ("Hello".equals(greeting)) {
 			System.out.println("Equals");
 		}
-		
+
 		String empty = "";
-		
+
 		if (empty.length() == 0)
 			System.out.println("Length of String is 0");
-			
+
 		if (empty.equals(""))
 			System.out.println("String equals \"\"");
-			
+
 		if (empty != null && empty.length() != 0)
 			System.out.println("String is not null and its length is not 0. String: " + empty);
 
-
 	}
-
 
 }

e774ef24fe44bae6a66102e144d68e1be029b53d
Bernd Hollerit <hollerit@gmail.com>
2021-06-24 16:39:33 +0900

    Add Message

---
+++ corejava/bernd/Message/Message.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/**
+ * Exercise of Core Java Chapter 3, page 112f
+ * 
+ * @version 1.00 2021-06-24
+ * @author Bernd Hollerit
+ */
+// Call this program with parameters, e.g.:
+// java Message -g cruel world
+public class Message
+{
+	public static void main(String[] args)
+	{
+		if (args.length == 0 || args[0].equals("-h"))
+			System.out.print("Hello,");
+		else if (args[0].equals("-g"))
+			System.out.print("Goodbye,");
+		for (int i=1; i<args.length; i++)
+			System.out.print(" " + args[i]);
+		System.out.print("!");		
+	}
+}

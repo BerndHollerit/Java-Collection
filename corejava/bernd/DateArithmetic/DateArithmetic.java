7f0f5d6bdc2a8e211695f86ab475ae20e81804f2
Bernd Hollerit <hollerit@gmail.com>
2021-07-01 15:55:57 +0900

    Implement calendar print functionality

--- corejava/bernd/DateArithmetic/DateArithmetic.java
+++ corejava/bernd/DateArithmetic/DateArithmetic.java
@@ -1,3 +1,4 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Arrays;
 
@@ -20,6 +21,15 @@ public class DateArithmetic
 		String[] weekDays = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 		System.out.println(Arrays.toString(weekDays));
 		
+		
+		LocalDate date = LocalDate.now();
+		int month = date.getMonthValue();
+		int today = date.getDayOfMonth();
+		
+		date = date.minusDays(today -1);
+		DayOfWeek weekday = date.getDayOfWeek();
+		int value = weekday.getValue();
+		
 		for (int i=0; i<weekDays.length; i++)
 		{
 			System.out.print(weekDays[i]);
@@ -29,5 +39,33 @@ public class DateArithmetic
 			}
 		}
 		System.out.println();
+		
+		for (int i=1; i<value; i++)
+			System.out.print("    ");
+		
+//		for (date.getDayOfMonth(); date.getDayOfMonth()<=31; date.plusDays(1))
+//		{
+//			if (date.getDayOfMonth()<10)
+//				System.out.print("  " + date.getDayOfMonth());
+//			else
+//				System.out.print(" " + date.getDayOfMonth());
+//			
+//			if(date.getDayOfWeek().getValue()%7==0)
+//				System.out.println();
+//				
+//		}
+		
+		while (date.getMonthValue() == month)
+		{
+			System.out.printf("%3d", date.getDayOfMonth());
+			if (date.getDayOfMonth() != today)
+				System.out.print(" ");
+			else
+				System.out.print("*");
+			date = date.plusDays(1);
+			if (date.getDayOfWeek().getValue() == 1)
+				System.out.println();
+		}
+		
 	}
 }
